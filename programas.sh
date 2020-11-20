44b5dd44cd8e732adedfe245f63bbb6c5e80f759
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2020-11-20 10:01:57 -0300

    Update programas.sh

--- programas.sh
+++ programas.sh
@@ -8,5 +8,12 @@ apt install imagemagick rsyslog  btrfs-progs xfsprogs xfsdump gdebi wget git cur
 apt install simplescreenrecorder bleachbit speedtest-cli p7zip p7zip-full p7zip-rar lzma lzma-dev rar unrar-free ark ncompress rzip file-roller -y
 apt install zip unzip xarchiver poppler-utils ttf-mscorefonts-installer stress-ng zsh  atop xsensors jpegoptim steghide  smbclient -y
 apt install cpu-checker bridge-utils virtinst virt-viewer  qemu qemu-kvm virt-manager libvirt-daemon nmap -y 
-
-
+echo "Finalizando" 
+sleep 2
+clear
+kvm-ok 
+lsmod | grep -i kvm 
+systemctl enable libvirtd --now
+adduser $USER kvm
+adduser $USER libvirt
+ 

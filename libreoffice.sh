3d6c5afc185e97cfc91efda31f0d724f5bceac33
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2021-02-09 09:04:13 -0300

    Update libreoffice.sh

--- libreoffice.sh
+++ libreoffice.sh
@@ -5,5 +5,6 @@ echo "Instalando LibreOffice"
 sleep 2
 add-apt-repository ppa:libreoffice/libreoffice-7-0
 apt update --fix-missing && apt install libreoffice -y 
+apt install libreoffice-l10n-pt-br -y
 clear
 echo "Libreoffice instalado com sucesso!"

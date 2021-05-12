6481e2504b14df3c7e50bdd24dd32ff8945f78c0
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2021-05-12 17:43:20 -0300

    Update programas.sh

--- programas.sh
+++ programas.sh
@@ -2,13 +2,14 @@
 #
 echo "Instalando os programas"
 sleep 2
-apt install wipe nwipe dc3dd dcfldd clonezilla foremost testdisk gddrescue growisofs screenfetch scrot unoconv tilix htop gparted secure-delete -y
-apt install samba samba-common libcups2 sshfs openssh-server ntp ntpdate python   vim mc feh ffmpeg  -y
-apt install imagemagick rsyslog  btrfs-progs xfsprogs xfsdump gdebi wget git curl redshift redshift-gtk audacious audacity vlc -y
-apt install simplescreenrecorder bleachbit speedtest-cli p7zip p7zip-full p7zip-rar lzma lzma-dev rar unrar-free ark ncompress rzip file-roller -y
-apt install zip unzip xarchiver poppler-utils ttf-mscorefonts-installer stress-ng zsh  atop xsensors jpegoptim steghide  smbclient -y
+apt install wipe nwipe  dcfldd clonezilla foremost testdisk gddrescue growisofs screenfetch scrot unoconv tilix htop gparted secure-delete -y
+apt install samba samba-common libcups2 sshfs openssh-server ntp ntpdate python vim mc feh   -y
+apt install  rsyslog  btrfs-progs xfsprogs xfsdump gdebi wget git curl redshift redshift-gtk audacious audacity vlc -y
+apt install simplescreenrecorder  file-roller -y
+#apt install  p7zip p7zip-full p7zip-rar lzma lzma-dev  ark ncompress rzip -y
+apt install zip unzip rar unrar-free  poppler-utils ttf-mscorefonts-installer stress-ng zsh  atop xsensors jpegoptim steghide ncdu  smbclient -y
 apt install nmap progress build-essential  gvfs-backends nfs-kernel-server nfs-common hddtemp smartmontools  -y 
-apt install cpu-checker bridge-utils virtinst virt-viewer  qemu qemu-kvm virt-manager libvirt-daemon ncdu -y
+#apt install cpu-checker bridge-utils virtinst virt-viewer  qemu qemu-kvm virt-manager libvirt-daemon ncdu -y
 #apt install virtualbox  virtualbox-guest-additions-iso -y 
 echo "Finalizando" 
 sleep 2

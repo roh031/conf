e5a7a426d510903b130154962f49c62dcf77f82c
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2021-08-29 08:57:36 -0300

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -3,5 +3,7 @@
 #
 echo "Instalando SHELL Zsh"
 sleep 2
+sudo apt install git curl wget zsh -y 
+cd ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 

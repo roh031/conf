c1b9d81e4ce87ddb00874662ffbf0e5fa6413a7d
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2021-08-29 08:55:06 -0300

    Add files via upload
    Configuração do VIM

---
+++ VimConfig.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+echo "Criando o arquivo de configuração do VIM" 
+sleep 2 
+apt install vim -y 
+echo "set number" > ~/.vimrc
+echo "map q :quit<CR>" >> ~/.vimrc
+echo "map <C-s> :wq<CR>" >> ~/.vimrc
+echo "set incsearch" >> ~/.vimrc
+echo "set wildmenu" >> ~/.vimrc
+echo "set laststatus=2" >> ~/.vimrc
+echo "set confirm" >> ~/.vimrc
+echo "color murphy" >> ~/.vimrc
+echo "stty -ixon" >> ~/.zshrc
+source ~/.zshrc
+#
+#
+echo "Concluído"
+sleep 2

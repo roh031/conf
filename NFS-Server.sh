c81d34a6fe03094bf300824123277381e2cd9d60
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2021-08-29 09:01:48 -0300

    Update NFS-Server.sh

--- NFS-Server.sh
+++ NFS-Server.sh
@@ -5,5 +5,4 @@ echo "Instalando Servidor NFS"
 sleep 2 
 apt install nfs-kernel-server  nfs-common -y 
 clear
-echo "Edite o arquivo de configuração em '/etc/exports'"
-echo "#caminho_da_pasta_a_ser_compartilhada		192.168.1.0/24(rw,subtree_check,insecure,sync,no_root_squash)"
+echo "~/192.168.1.0/24              (rw,subtree_check,insecure,sync,no_root_squash)" >> /etc/exports

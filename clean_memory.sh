143c78f2f6182e530251d72dedb7aec470a0e6e3
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2021-08-31 19:22:14 -0300

    Update clean_memory.sh

--- clean_memory.sh
+++ clean_memory.sh
@@ -1,1 +1,6 @@
-sync && swapoff -a && sysctl vm.swappiness=5 && sysctl vm.vfs_cache_pressure=50 && sysctl vm.drop_caches=3 && swapon -a && sync
+swapoff -a 
+sysctl vm.swappiness=5 
+sysctl vm.vfs_cache_pressure=50 
+sysctl vm.drop_caches=3 
+swapon -a
+sync

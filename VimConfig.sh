263b682a91a013b0d8f81e1fb4d0514bd71ad0bb
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2021-08-30 21:14:40 -0300

    Update VimConfig.sh

--- VimConfig.sh
+++ VimConfig.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
-echo "Criando o arquivo de configuração do VIM" 
-sleep 2 
+echo "Criando o arquivo de configuração do VIM"
+sleep 2
+apt install vim -y
 echo "set number" > ~/.vimrc
 echo "map q :quit<CR>" >> ~/.vimrc
 echo "map <C-s> :wq<CR>" >> ~/.vimrc
@@ -11,9 +12,9 @@ echo "set laststatus=2" >> ~/.vimrc
 echo "set confirm" >> ~/.vimrc
 echo "color murphy" >> ~/.vimrc
 echo "stty -ixon" >> ~/.zshrc
+echo "stty -ixon" >> ~/.bashrc
+source ~/.bashrc
 source ~/.zshrc
-echo 'stty -ixon' >> ~/.bashrc
-source ~/.bashrc 
 #
 #
 echo "Concluído"

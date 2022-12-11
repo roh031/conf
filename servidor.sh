2bfa9c4e6942b31cb77f6e0b1632c044f06ebd68
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2022-12-11 08:29:14 -0300

    Update servidor.sh

--- servidor.sh
+++ servidor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Instalando os pacotes"
 sleep 2
-apt install wipe nwipe  dcfldd clonezilla foremost testdisk gddrescue growisofs  scrot unoconv hashdeep  -y 
+apt install wipe nwipe  dcfldd clonezilla foremost testdisk gddrescue growisofs  scrot unoconv   -y 
 apt install samba samba-common libcups2 sshfs openssh-server ntp ntpdate python anacron -y 
 apt install rsyslog  btrfs-progs xfsprogs xfsdump gdebi wget git curl screenfetch ncdu -y 
-apt install speedtest-cli  rar unrar-free htop nfs-kernel-server nfs-common hddtemp progress build-essential  -y 
+apt install speedtest-cli  rar unrar-free htop  gvfs-backends nfs-kernel-server nfs-common  progress build-essential  -y 
 apt install poppler-utils  stress-ng zsh  atop  jpegoptim squid3 secure-delete zip unzip  -y
 

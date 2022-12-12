7ff321441996d9d3b01b4d15957089ce3b708673
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2022-12-12 05:46:02 -0300

    Update trimSSD.sh

--- trimSSD.sh
+++ trimSSD.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Instale o ANACRON e adicione a seguinte linha de comando:
+# @monthly	5	TrimSSD	/home/ronaldo/Documentos/conf/trimSSD.sh
 LOG=/var/log/trim.log
 echo "*** $(date -R) ***" >> $LOG
 fstrim -v / >> $LOG

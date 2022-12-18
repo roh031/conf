800afa12010d379e2c72d589a73e2f1b7fa682af
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2022-12-18 10:45:02 -0300

    Update sambashare-resolv.sh

--- sambashare-resolv.sh
+++ sambashare-resolv.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#sudo mkdir /var/lib/samba/usershares
+#groupadd -r sambashare
 sudo chown root:sambashare /var/lib/samba/usershares
 sudo chmod 1770 /var/lib/samba/usershares
 sudo usermod -aG sambashare $USER

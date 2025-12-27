16e14ddf3095946c10320f1304a979d25cc89c16
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2025-12-27 20:10:17 -0300

    Delete batery.sh

--- batery.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-echo "Instalando gerenciador de bateria"
-sleep 2
-sudo add-apt-repository ppa:slimbook/slimbook
-sudo apt-get update
-sudo apt-get install slimbookbattery -y

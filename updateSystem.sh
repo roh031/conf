229ed0074ca3dfdf73be714482edb7742fca35e3
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2025-12-27 20:07:14 -0300

    Update updateSystem.sh

--- updateSystem.sh
+++ updateSystem.sh
@@ -5,7 +5,7 @@ LOG=/var/log/updateSYSTEM.log
 apt update >> $LOG
 apt upgrade -y >> $LOG
 apt install -f -y >> $LOG
-apt auotemove -y 
+apt autoremove -y 
 apt clean && apt autoclean
 freshclam >> $LOG
 flatpak update >> $LOG

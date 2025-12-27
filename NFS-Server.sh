aeb984cfde94382d252c56206981e27b53795c7a
RONALDO S  ARAUJO <48135044+roh031@users.noreply.github.com>
2025-12-27 20:09:02 -0300

    Delete NFS-Server.sh

--- NFS-Server.sh
+++
@@ -1,8 +0,0 @@
-!/usr/bin/env bash 
-#
-#
-echo "Instalando Servidor NFS"
-sleep 2 
-apt install nfs-kernel-server  nfs-common -y 
-clear
-echo "~/192.168.1.0/24              (rw,subtree_check,insecure,sync,no_root_squash)" >> /etc/exports

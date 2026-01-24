001441323d4dbf755394b550d86bba754bec5803
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-01-24 16:12:13 -0300

    Update servidor_orangepi.sh

--- servidor_orangepi.sh
+++ servidor_orangepi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla zsh git curl wget testdisk growisofs rsyslog screenfetch dcfldd speedtest-cli zip unzip clamav mdadmn -y 
+apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla zsh git curl wget testdisk growisofs rsyslog screenfetch dcfldd speedtest-cli zip unzip clamav mdadm -y 
 chown root:sambashare /var/lib/samba/usershares
 chmod 1770 /var/lib/samba/usershares
 adduser orangepi sambashare

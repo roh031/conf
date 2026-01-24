87ebde16dc73cec105edd4f70996a728faf863c9
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-01-24 16:08:56 -0300

    Delete tailscale_exitMode.sh

--- tailscale_exitMode.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-echo "Habilitando o Exit Mode"
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
-#
-sudo tailscale set --advertise-exit-node

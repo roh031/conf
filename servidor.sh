361558db2798523c200a19106c3fd9110d4e381d
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-01-24 16:07:54 -0300

    Update servidor.sh

--- servidor.sh
+++ servidor.sh
@@ -3,9 +3,9 @@
 apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla zsh git curl wget testdisk growisofs rsyslog screenfetch dcfldd speedtest-cli zip unzip -y 
 chown root:sambashare /var/lib/samba/usershares
 chmod 1770 /var/lib/samba/usershares
-adduser ronaldo sambashare
+adduser $USER sambashare
 chown root:sambashare /var/lib/samba/usershares
 chmod 1770 /var/lib/samba/usershares
-usermod -aG sambashare ronaldo
-smbpasswd -a ronaldo
-systemctl restart smbd.service
+usermod -aG sambashare $USER
+smbpasswd -a $USER
+systemctl restart smbd.service nmbd.service

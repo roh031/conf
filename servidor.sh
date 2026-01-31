2ec058da0bdd58d8ce53ad2571d4b80120a63832
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-01-31 10:20:15 -0300

    Update servidor.sh

--- servidor.sh
+++ servidor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla zsh git curl wget testdisk growisofs mdadm rsyslog neofetch dcfldd speedtest-cli zip unzip -y 
+sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla zsh git curl wget testdisk growisofs mdadm rsyslog neofetch dcfldd speedtest-cli zip unzip docker.io docker-compose -y 
 sudo chown root:sambashare /var/lib/samba/usershares
 sudo chmod 1770 /var/lib/samba/usershares
 sudo adduser $USER sambashare

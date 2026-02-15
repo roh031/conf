96b6badfa329b77b523e2717513075e098223eb1
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-02-15 09:40:12 -0300

    Update servidor_orangepi.sh

--- servidor_orangepi.sh
+++ servidor_orangepi.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 #
-apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla zsh git curl wget testdisk growisofs rsyslog neofetch dcfldd speedtest-cli zip unzip clamav mdadm -y 
-chown root:sambashare /var/lib/samba/usershares
-chmod 1770 /var/lib/samba/usershares
-adduser orangepi sambashare
-chown root:sambashare /var/lib/samba/usershares
-chmod 1770 /var/lib/samba/usershares
-usermod -aG sambashare orangepi
-smbpasswd -a orangepi
-systemctl restart smbd.service nmbd.service
+sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla zsh git curl wget testdisk growisofs rsyslog neofetch dcfldd speedtest-cli zip unzip clamav mdadm -y
+sudo apt install docker.io docker-compose containerd -y
+sudo usermod -aG docker $USER
+sudo systemctl enable --now docker docker.socket containerd
+sudo chown root:sambashare /var/lib/samba/usershares
+sudo chmod 1770 /var/lib/samba/usershares
+sudo adduser orangepi sambashare
+sudo chown root:sambashare /var/lib/samba/usershares
+sudo chmod 1770 /var/lib/samba/usershares
+sudo usermod -aG sambashare orangepi
+sudo smbpasswd -a orangepi
+sudo systemctl restart smbd.service nmbd.service

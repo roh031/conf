b40a8feece07519d6d24c2d45f2bc60822a3fb5a
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-02-15 10:00:03 -0300

    Update trimSSD.sh

--- trimSSD.sh
+++ trimSSD.sh
@@ -3,5 +3,5 @@
 # @monthly	5	TrimSSD	/home/ronaldo/Documentos/conf/trimSSD.sh
 LOG=/var/log/trim.log
 echo "*** $(date -R) ***" >> $LOG
-fstrim -v / >> $LOG
-fstrim -v /home >> $LOG
+sudo fstrim -v / >> $LOG
+sudo fstrim -v /home >> $LOG

5c67ebdf40bc9a644f9ab47f5f96837d5df7428a
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-04-03 09:44:36 -0300

    Update servidor.sh

--- servidor.sh
+++ servidor.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 #
-sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla zsh git curl wget testdisk growisofs mdadm rsyslog neofetch dcfldd speedtest-cli zip unzip 
+sudo apt install zsh -y
+cd $HOME
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla  git curl wget testdisk growisofs mdadm rsyslog neofetch speedtest-cli zip unzip unrar-free
 sudo apt install docker.io docker-compose containerd dcfldd ufw -y
 sudo usermod -aG docker $USER
 sudo systemctl enable --now docker.socket docker containerd
@@ -9,9 +12,6 @@ sudo chmod 1770 /var/lib/samba/usershares
 sudo adduser $USER sambashare
 sudo usermod -aG sambashare $USER
 sudo smbpasswd -a $USER
-sudo systemctl restart smbd.service nmbd.service rsyslog.service
+sudo systemctl restart smbd.service nmbd.service
 sudo systemctl enable --now fstrim.timer
-sudo apt install zsh -y
-cd $HOME
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo reboot now

571d7b5fa996d494cf5605c96c596b119fb63514
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-04-17 09:04:10 -0300

    Atualizar o servidor.sh

--- servidor.sh
+++ servidor.sh
@@ -4,12 +4,11 @@ sudo apt install zsh -y
 sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla  git curl wget testdisk growisofs mdadm neofetch speedtest-cli zip unzip unrar-free
 sudo apt install docker.io docker-compose containerd dcfldd ufw -y
 sudo usermod -aG docker $USER
-sudo systemctl enable --now docker.socket docker containerd
+sudo systemctl enable --now docker.socket docker containerd fstrim.timer
 sudo chown root:sambashare /var/lib/samba/usershares
 sudo chmod 1770 /var/lib/samba/usershares
 sudo adduser $USER sambashare
 sudo usermod -aG sambashare $USER
 sudo smbpasswd -a $USER
 sudo systemctl restart smbd.service nmbd.service
-sudo systemctl enable --now fstrim.timer
 sudo reboot now

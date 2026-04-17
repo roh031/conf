5069d1297dfc50c40946a081b17373e3f199ecee
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-04-17 09:03:06 -0300

    Atualizar o servidor.sh

--- servidor.sh
+++ servidor.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 #
 sudo apt install zsh -y
-cd $HOME
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla  git curl wget testdisk growisofs mdadm rsyslog neofetch speedtest-cli zip unzip unrar-free
+sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla  git curl wget testdisk growisofs mdadm neofetch speedtest-cli zip unzip unrar-free
 sudo apt install docker.io docker-compose containerd dcfldd ufw -y
 sudo usermod -aG docker $USER
 sudo systemctl enable --now docker.socket docker containerd

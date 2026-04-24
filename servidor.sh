00e1a5030a75d27291c0e13cf3772b656c33c721
RONALDO ARAUJO <48135044+roh031@users.noreply.github.com>
2026-04-24 19:44:44 -0300

    Update servidor.sh

--- servidor.sh
+++ servidor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 sudo apt update
-sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla  git curl wget testdisk growisofs mdadm neofetch speedtest-cli zip unzip unrar-free zsh -y
+sudo apt install samba samba-common libcups2 smbclient htop gddrescue foremost wipe nwipe clonezilla  git curl wget testdisk growisofs mdadm speedtest-cli zip unzip unrar-free zsh -y
 sudo apt install docker.io docker-compose containerd dcfldd ufw -y
 sudo usermod -aG docker $USER
 sudo systemctl enable --now docker.socket docker containerd fstrim.timer
@@ -11,4 +11,4 @@ sudo adduser $USER sambashare
 sudo usermod -aG sambashare $USER
 sudo smbpasswd -a $USER
 sudo systemctl restart smbd.service nmbd.service
-sudo reboot now
+#sudo reboot now
